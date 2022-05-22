d6ca3ccd46340b9d4c17378e4ee6901dd1d8313e
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-05-22 12:29:30 +0600

    basic setup and mongodb connected done

---
+++ index.js
@@ -0,0 +1,96 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+//midlleware 
+app.use(cors());
+app.use(express.json());
+
+//==============main part=================
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hduli.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+/* ======================================================= */
+async function run(){
+    try{
+      
+    }
+    finally{
+        
+    }
+
+}
+
+run().catch(console.dir);
+
+
+//==============main part=================
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hey i m  running and waiting for you')
+})
+
+
+app.listen(port,()=>{
+    console.log('server is running in runnig ',port);
+})
+
+
+
+
+// after porcees 
+
+/* 
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+//midlleware 
+app.use(cors());
+app.use(express.json());
+
+//================Connet to Cluster============== 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8c3ja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+        
+    }
+
+}
+
+run().catch(console.dir);
+
+
+//==============================================
+
+
+app.get('/',(req,res)=>{
+    res.send('john is running and waiting for you')
+})
+
+
+app.listen(port,()=>{
+    console.log('john is running in runnig ',port);
+})
+
+
+*/
