fc63af93ad6c300857db3052ce5b56cd4c78cc9b
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-05-25 04:28:05 +0600

    admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb')
 const express = require('express');
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const port = process.env.PORT || 4000;
 
@@ -16,12 +17,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hduli.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+  
+  /* =================== */
 /* ======================================================= */
 async function run(){
     try{
         await client.connect();
         const partsCollection = client.db('manufracter').collection('parts');
         const reviewsCollection = client.db('manufracter').collection('review');
+        const userCollection = client.db('manufracter').collection('users');
         
 
  
@@ -34,6 +52,47 @@ async function run(){
             res.send(parts)
           }); 
 
+
+
+          app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();  
+            res.send(users);
+          });
+        //   users 
+
+        app.put('/user/admin/:email',verifyJWT, async (req, res) => {  
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({email: email});
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }; 
+            const updateDoc = { 
+              $set: {role:'admin'},
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+           res.send(result);
+            }
+            else{
+                res.status(403).send({message:'forbidden'});
+            }
+            
+          });
+
+          //admin
+          app.put('/user/:email', async (req, res) => {  
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email }; 
+            const options = { upsert: true };
+            const updateDoc = { 
+              $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+          const token =jwt.sign({email:email},process.env.ACCECSS_TOKEN_SECRET,{expiresIn: '1h'})
+            res.send({result,token});
+          });
+  
+   
           /* add products */
           app.post('/parts',async (req,res)=>{
             const newParts= req.body;
